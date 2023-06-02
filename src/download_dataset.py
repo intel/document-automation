2619eb6473cb258098392220c096ac9f2806b02f
Zheng Cai <zheng.cai@intel.com>
2023-06-02 10:48:29 +0800

    refine

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,3 +1,4 @@
+import argparse
 from dataset_librarian.dataset_api.download import download_dataset
 from dataset_librarian.dataset_api.preprocess import preprocess_dataset
 
@@ -6,7 +7,7 @@ def parse_cmd():
     args = argparse.ArgumentParser(description=desc, epilog=' ', formatter_class=argparse.RawTextHelpFormatter)
     
      # directories
-    args.add_argument('--dataset_dir', type=str, default='/home/user/dataset/', dest='dataset_dir')
+    args.add_argument('--dataset_dir', type=str, dest='dataset_dir')
  
     print(args.parse_args())
     return args.parse_args()
